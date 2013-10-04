38774c44e6d33db805f6330427dad55b24e86574
Jon Fazzaro <jon@fazzaro.com>
2013-10-04 14:54:46 -0400

    Initial check-in.

---
+++ mvc-spa/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="mvc_spa.MvcApplication" Language="C#" %>
---
+++ mvc-spa/Scripts/routing.js
@@ -0,0 +1,20 @@
+﻿var Routing = function (appRoot, contentSelector, defaultRoute) {
+
+    function getUrlFromHash(hash) {
+        var url = hash.replace('#/', '');
+        if (url === appRoot)
+            url = defaultRoute;
+        return url;
+    }
+
+    return {
+        init: function () {
+            Sammy(contentSelector, function () {
+                this.get(/\#\/(.*)/, function (context) {
+                    var url = getUrlFromHash(context.path);
+                    context.load(url).swap();
+                });
+            }).run('#/');
+        }
+    };
+}
